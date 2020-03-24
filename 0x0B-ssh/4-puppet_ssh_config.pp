83cd2bf808c944c4d25835be290a9a96dcb87ce6
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-03-24 22:33:20 +0000

    Fix in line

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 # Client configuration file (w/ Puppet)
 
 file_line { 'Turn off passwd auth'
-line => 'PasswordAuthentication no',
+line => ' PasswordAuthentication no',
 path => '/etc/ssh/ssh_config'
 }
 
 file_line { 'Declare identity file'
-line => 'IdentityFile ~/.ssh/holberton',
+line => ' IdentityFile ~/.ssh/holberton',
 path => '/etc/ssh/ssh_config'
 }
