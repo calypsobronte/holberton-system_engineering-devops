505334c7e78d88ca9bd738b41204df557d2d5cab
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-04-04 02:33:54 +0000

    [puppet_custom_http_response-header] 2. Add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,6 @@
+# Using Puppet
+
+exec { 'Install nginx':
+  command  => "sudo apt-get -y update && sudo apt-get -y install nginx && echo 'Holberton School' > sudo /var/www/html/index.html && sudo sed -i '60 i\ \tadd_header X-Served-By $HOSTNAME;' /etc/nginx/nginx.conf && sudo service nginx restart",
+  provider => 'shell',
+}
