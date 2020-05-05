f7209f7f5496ca11b85d8a3d7518d7384c735911
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-05 04:53:52 +0000

    Fix in code and style pep8

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,14 +5,15 @@ import json
 import requests
 
 if __name__ == '__main__':
-    url_todos = "https://jsonplaceholder.typicode.com/users/{}/todos"
-    url_names = "https://jsonplaceholder.typicode.com/users/{}"
-    name_users = requests.get(url_names).json()
+    url_todos = "https://jsonplaceholder.typicode.com/users"
+    name_users = requests.get(url_todos, verify=False).json()
     list_tasks = {}
     for user in name_users:
-        api_todos = requests.get(url_todos.format(user['id'])).json()
-        list_tasks[user['id']] = [{'task': task.get('title'), 'completed':
-                  task.get('completed'), 'username': name_users['username']}
-                  for task in api_todos]
+        api_todos = requests.get(url_todos + '/{}/todos'
+                                 .format(user['id'])).json()
+        list_tasks[user['id']] = [{'task': task.get('title'),
+                                   'completed': task.get('completed'),
+                                   'username': user['username']}
+                                  for task in api_todos]
     with open("todo_all_employees.json", 'w', newline='') as jsonfile:
         json.dump(list_tasks, jsonfile)
