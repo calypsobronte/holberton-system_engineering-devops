94b051b539fe5b06ba609fb0cc900d9a86fdb317
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-05 03:30:05 +0000

    Fix styles

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,8 +6,12 @@ from sys import argv
 
 if __name__ == '__main__':
     user_id = argv[1]
-    api_todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)).json()
-    name_users = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(user_id)).json()
-    list_tasks = [task.get('title') for task in api_todos if task.get('completed')]
-    print("Employee {} is done with tasks({}/{}):".format(name_users.get('name'), len(list_tasks), len(api_todos)))
+    url_todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'
+    api_todos = requests.get(url_todos.format(user_id)).json()
+    url_names = 'https://jsonplaceholder.typicode.com/users/{}'
+    name_users = requests.get(url_names.format(user_id)).json()
+    list_tasks = [task.get('title') for task in api_todos
+                  if task.get('completed')]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name_users.get('name'), len(list_tasks), len(api_todos)))
     [print("\t {}".format(task)) for task in list_tasks]
