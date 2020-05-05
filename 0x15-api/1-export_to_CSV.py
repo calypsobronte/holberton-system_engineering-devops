e7a1cc3ba6ba403b79c2600ac50469fe4fe895aa
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-05 03:55:36 +0000

    Fix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ Python CVS """
 
+import csv
 import requests
 from sys import argv
-import csv
 
 if __name__ == '__main__':
     user_id = argv[1]
-    url_todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'
-    api_todos = requests.get(url_todos.format(user_id)).json()
-    url_names = 'https://jsonplaceholder.typicode.com/users/{}'
-    name_users = requests.get(url_names.format(user_id)).json()
+    url_todos = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    api_todos = requests.get(url_todos.format(user_id), verify=False).json()
+    url_names = "https://jsonplaceholder.typicode.com/users/{}"
+    name_users = requests.get(url_names.format(user_id), verify=False).json()
     with open("{}.csv".format(user_id), 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in api_todos:
