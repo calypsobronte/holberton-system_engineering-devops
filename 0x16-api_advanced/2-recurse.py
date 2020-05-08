12bef8ecaf6e4b7cd03d585767746d3831ea3687
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-08 03:06:05 +0000

    function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""API reddit """
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit, after)
+    headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
+                         rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
+    with requests.session() as client:
+        info = client.get(url, headers=headers, allow_redirects=False).json()
+        try:
+            for i in info.get('data').get('children'):
+                hot_list.append(i.get("data").get("title"))
+            return hot_list + recurse(subreddit, [], info.get("data").get("after"))
+        except Exception:
+            if after is None:
+                return []
