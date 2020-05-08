1f6b64ce41e184d0ba919d0177c507bc5abfa34e
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-08 03:36:41 +0000

    Fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,18 +3,20 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
+def recurse(subreddit, hot_list=[], after=''):
     url = 'https://www.reddit.com/r/{}/hot.json?\
            after={}'.format(subreddit, after)
     headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
                          rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
     with requests.session() as client:
-        info = client.get(url, headers=headers, allow_redirects=False).json()
         try:
-            for i in info.get('data').get('children'):
-                hot_list.append(i.get("data").get("title"))
+            info = client.get(url, headers=headers, allow_redirects=False).json()
+            child = info.get('data').get('children')
+            after = info.get('data').get('after')
+            for i in child:
+                hot_list.append(i.get('data').get('title'))
             if after is None:
                 return hot_list
-            return recurse(subreddit, hot_list, info.get("data").get("after"))
+            return recurse(subreddit, hot_list, after)
         except Exception:
             return None
