aa33bd09d663427e62cd61cfc880d7a76d4ef31a
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-08 03:42:03 +0000

    Fix delete with

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,15 +8,14 @@ def recurse(subreddit, hot_list=[], after=''):
            after={}'.format(subreddit, after)
     headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
                          rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
-    with requests.session() as client:
-        try:
-            info = client.get(url, headers=headers, allow_redirects=False).json()
-            child = info.get('data').get('children')
-            after = info.get('data').get('after')
-            for i in child:
-                hot_list.append(i.get('data').get('title'))
-            if after is None:
-                return hot_list
-            return recurse(subreddit, hot_list, after)
-        except Exception:
-            return None
+    try:
+        info = requests.get(url, headers=headers, allow_redirects=False).json()
+        child = info.get('data').get('children')
+        after = info.get('data').get('after')
+        for i in child:
+            hot_list.append(i.get('data').get('title'))
+        if after is None:
+            return hot_list
+        return recurse(subreddit, hot_list, after)
+    except Exception:
+        return None
