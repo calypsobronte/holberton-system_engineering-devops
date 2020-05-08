ffeef4c619242abcfcca009cca288e4e72a589f1
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-08 03:11:47 +0000

    Fix in line pep8 style

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,7 +4,8 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit, after)
+    url = 'https://www.reddit.com/r/{}/hot.json?\
+           after={}'.format(subreddit, after)
     headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
                          rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
     with requests.session() as client:
@@ -12,7 +13,8 @@ def recurse(subreddit, hot_list=[], after=""):
         try:
             for i in info.get('data').get('children'):
                 hot_list.append(i.get("data").get("title"))
-            return hot_list + recurse(subreddit, [], info.get("data").get("after"))
+            return hot_list + recurse(subreddit, [],
+                                      info.get("data").get("after"))
         except Exception:
             if after is None:
                 return []
