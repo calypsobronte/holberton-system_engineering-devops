d33b205eedd16842400d536b1020b8a11aad0faf
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-05-20 22:43:07 +0000

    Fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #  Strace is your friend
-exec { 'sed config':
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+exec { 'sed-config':
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   path    => '/bin/'
 }
